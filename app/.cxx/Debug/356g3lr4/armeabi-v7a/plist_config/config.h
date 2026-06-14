3ddec33b9ff2378c2a24f165f7c865561ab8238a
Maxim Ukhin <ukhin.maxim@gmail.com>
2026-06-14 14:52:41 +0300

    Remove build cache files

--- app/.cxx/Debug/356g3lr4/armeabi-v7a/plist_config/config.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#define HAVE_STDINT_H 1
-#define HAVE_STDLIB_H 1
-#define HAVE_STRING_H 1
-#define HAVE_WCHAR_H 1
-#define HAVE_BYTESWAP_H 1
-#define PACKAGE "libplist"
-#define PACKAGE_VERSION "2.7.0"
